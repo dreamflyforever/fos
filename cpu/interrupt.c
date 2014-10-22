049103e24259a94a7c4e68694a2a4fa54c27ae27
Shanjin Yang <sjyang@credosemi.com>
2014-10-22 16:51:24 +0800

    Remove the zero time delay of interrupt method, because it have a bug.
    Signed-off-by: Shanjin Yang <sjyang@credosemi.com>

--- cpu/interrupt.c
+++ cpu/interrupt.c
@@ -1,8 +1,20 @@
+/*
+ * Copyright (c) 2013, Shanjin Yang.<sjyangv0@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of the FOS.
+ *
+ * The latest version of FOS download by <https://github.com/yangshanjin/YSJ_OS>
+ *
+ */
+
 #include "../kernel/include/var_define.h"
 #include "include/hw_include.h"
 
+#if 0
 U32 int_stack[32][4*100];
 TCB int_tcb[32];
+#endif
 
 /* Disable or1200 interrupt */
 U32 interrupt_disable(void)
@@ -11,6 +23,7 @@ U32 interrupt_disable(void)
  
 	cpu_sr = mfspr(SPR_SR);
 	mtspr(SPR_SR, cpu_sr & ~(SPR_SR_IEE | SPR_SR_TEE));
+
 	return cpu_sr;
 }
 
@@ -19,7 +32,6 @@ void interrupt_enable(U32 cpu_sr)
 {
 	mtspr(SPR_SR, cpu_sr); /* | (SPR_SR_IEE | SPR_SR_TEE) */
 }
-
 
 /* Interrupt handlers table */
 struct ihnd int_handlers[MAX_INT_HANDLERS];
@@ -30,17 +42,18 @@ int hw_interrupt_init()
 	int i;
 		
 	for(i = 0; i < MAX_INT_HANDLERS; i++){
-		int_handlers[i].handler = 0;
-		int_handlers[i].arg = 0;
+		int_handlers[i].handler = NULL;
+		int_handlers[i].arg = NULL;
   	}
 
-  	mtspr(SPR_PICMR, 0x00000000);
-	//set OR1200 to accept exceptions
+    mtspr(SPR_PICMR, 0x00000000);
+	/* set OR1200 to accept exceptions */
 	mtspr(SPR_SR, mfspr(SPR_SR) | SPR_SR_IEE);
 	
 	return 0;
 }
 
+#if 0
 /* Add interrupt handler */ 
 int hw_interrupt_install_by_prio(unsigned long vect, void (* handler)(void *), void *prio)
 {
@@ -49,6 +62,21 @@ int hw_interrupt_install_by_prio(unsigned long vect, void (* handler)(void *), v
 
 	int_handlers[vect].handler = handler;
 	int_handlers[vect].arg = prio;
+
+	mtspr(SPR_PICMR, mfspr(SPR_PICMR) | (0x00000001L << vect));
+
+	return 0;
+}
+#endif
+
+/* Add interrupt handler */ 
+int hw_interrupt_install(unsigned long vect, void (* handler)(void *), void *arg)
+{
+	if(vect >= MAX_INT_HANDLERS)
+		return -1;
+
+	int_handlers[vect].handler = handler;
+	int_handlers[vect].arg = arg;
 
 	mtspr(SPR_PICMR, mfspr(SPR_PICMR) | (0x00000001L << vect));
 
@@ -70,7 +98,7 @@ int hw_interrupt_mask(unsigned long vect)
 int hw_interrupt_unmask(unsigned long vect)
 {
 	if(vect >= MAX_INT_HANDLERS)
-                return -1;
+        return -1;
 
 	mtspr(SPR_PICMR, mfspr(SPR_PICMR) | (0x00000001L << vect));
 
@@ -80,18 +108,19 @@ int hw_interrupt_unmask(unsigned long vect)
 /* Main interrupt handler */
 void usr_handle()
 {
-	unsigned long picsr = mfspr(SPR_PICSR);   //process only the interrupts asserted at signal catch, ignore all during process
-	unsigned long i = 0;
+    /* process only the interrupts asserted at signal catch, ignore all during process */
+	unsigned long picsr = mfspr(SPR_PICSR);
+    unsigned int i = 0;
 
-	while(i < 32) {
-		if((picsr & (0x01L << i)) && (int_handlers[i].handler != 0)) {
-            task_create(&int_tcb[i], int_handlers[i].handler, int_stack[i], *((U8 *)int_handlers[i].arg), 1);
-			//int_handlers[i].handler(int_handlers[i].arg); 
+    for (; i < MAX_INT_HANDLERS; i++){
+
+        if((picsr & (0x01L << i)) && (int_handlers[i].handler != NULL)) {
+		    
+            int_handlers[i].handler(int_handlers[i].arg); 
 		}
-		i++;
 	}
 
 	mtspr(SPR_PICSR, 0);	//clear interrupt status: all modules have level interrupts, which have to be cleared by software,
-					//thus this is safe, since non processed interrupts will get re-asserted soon enough
+					        //thus this is safe, since non processed interrupts will get re-asserted soon enough
 }
 
