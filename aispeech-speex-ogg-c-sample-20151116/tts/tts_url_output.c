98e37dbc1139d6df202485b981abb5f0dd1c9af3
liang.zhang <liang.zhang@aispeech.com>
2016-11-15 10:15:55 +0800

    improve tts function
    Signed-off-by: liang.zhang <liang.zhang@aispeech.com>

--- aispeech-speex-ogg-c-sample-20151116/tts/tts_url_output.c
+++ aispeech-speex-ogg-c-sample-20151116/tts/tts_url_output.c
@@ -51,25 +51,30 @@ char *url_decode(char *str) {
 	return buf;
 }
 
-char *tts_url_output(char *param)
+char *tts_param = "applicationId=146337845885959a&timestamp=1470721766&sig=c0bf68677e3324a4a9f5e35bbaa7b7e126b15dec&params={\"app\": {\"userId\": \"wifiBox\"}, \"audio\": {\"sampleBytes\": 2, \"sampleRate\": 16000, \"channel\": 1, \"audioType\": \"mp3\"}, \"request\": {\"speechVolume\": 50, \"coreType\": \"cn.sent.syn\", \"speechRate\": 0.85, \"rightMargin\": 5, \"realBack\": 1, \"res\": \"syn_chnsnt_zhilingf\",\"refText\": \"%s\"}}";
+
+char *tts_url = "http:/112.80.39.95:8009/cn.sent.syn/syn_chnsnt_zhilingf?";
+
+char *tts_url_output(char *cfg, char *param, char *text)
 {
-	char url[2048] = {0};
+	char *url = malloc(1024);
+        memset(url, 0, 215);
 	char buff[2048] = {0};
-	memset(url, 0, 2048);
 	int len;
-
-	char *text = url_encode("抱歉没找到刘德华的歌，请问您想听什么歌呢？");
-	sprintf(buff, "applicationId=146337845885959a&timestamp=1470721766&sig=c0bf68677e3324a4a9f5e35bbaa7b7e126b15dec&params={\"app\": {\"userId\": \"wifiBox\"}, \"audio\": {\"sampleBytes\": 2, \"sampleRate\": 16000, \"channel\": 1, \"audioType\": \"mp3\"}, \"request\": {\"speechVolume\": 50, \"coreType\": \"cn.sent.syn\", \"speechRate\": 0.85, \"rightMargin\": 5, \"realBack\": 1, \"res\": \"syn_chnsnt_zhilingf\",\"refText\": \"%s\"}}", text);
-	len = strlen(buff);
-	sprintf(url, "http:/112.80.39.95:8009/cn.sent.syn/syn_chnsnt_zhilingf?%s", buff);
+	char *t = url_encode(text);
+	sprintf(buff, param, t);
+        free(t);
+	sprintf(url, "%s%s", cfg, buff);
+	len = strlen(url);
+        printf("url len : %d\n", len);
 	printf("\n%s\n", url);
-
+        return url;
 }
-#if 0
-	sprintf(buff, "applicationId=146337845885959a&timestamp=1470721766&authId=57a96ee68827ad006a000001&sig=c0bf68677e3324a4a9f5e35bbaa7b7e126b15dec&params={\"app\": {\"userId\": \"wifiBox\"}, \"audio\": {\"sampleBytes\": 2, \"sampleRate\": 16000, \"compress\": \"raw\", \"channel\": 1, \"audioType\": \"mp3\"}, \"request\": {\"speechVolume\": 50, \"coreType\": \"cn.sent.syn\", \"speechRate\": 0.85, \"rightMargin\": 5, \"realBack\": 1, \"res\": \"syn_chnsnt_zhilingf\", \"leftMargin\": 5, \"refText\": \"%s\"}}", text);
-#endif
+
 int main()
 {
-	tts_url_output(NULL);
+        char *url;
+	url = tts_url_output(tts_url, tts_param, "抱歉没找到刘德华的歌，请问您想听什么歌呢？");
+        free(url);
 	return 0;
 }
